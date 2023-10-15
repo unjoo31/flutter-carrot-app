6760a8f16fa97bd5430891fca722e0b2dfaf1eba
unjoo31 <unjoo31@gmail.com>
2023-10-15 15:25:41 +0900

    "나의 당근화면 완료"

--- lib/screens/my_carrot/my_carrot_screen.dart
+++ lib/screens/my_carrot/my_carrot_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carrot_app/models/icon_menu.dart';
+import 'package:flutter_carrot_app/screens/my_carrot/components/card_icon_menu.dart';
+import 'package:flutter_carrot_app/screens/my_carrot/components/my_carrot_header.dart';
 
 class MyCarrotScreen extends StatelessWidget {
   const MyCarrotScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("myCarrotScreen"),
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text("나의 당근"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(
+            thickness: 0.5,
+            height: 0.5,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          MyCarrotHeader(),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu1),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu2),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu3),
+        ],
+      ),
     );
   }
 }
